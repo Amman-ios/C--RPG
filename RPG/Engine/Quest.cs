dbe23c6b4cbc218102305c3db42c152f9764cf94
Amman <samuel.crews0714@gmail.com>
2022-03-28 09:54:26 -0400

    L10

--- RPG/Engine/Quest.cs
+++ RPG/Engine/Quest.cs
@@ -13,6 +13,7 @@ namespace Engine
         public string Description { get; set; }
         public int RewardXP { get; set; }
         public int RewardGold { get; set; }
+        public Item RewardItem { get; set; }
 
         public Quest(int id, string name, string description, int rewardXP, int rewardGold)
         {
